9f7898b40f797529faa295ae0fbed6b982e3580c
Srinivas-K-61100 <66117584+Srinivas-K-61100@users.noreply.github.com>
2020-05-29 17:01:50 +0530

    Add files via upload

---
+++ jdoodle (1).java
@@ -0,0 +1,6 @@
+class B {
+    public static void main(String args[]) {
+     System.out.println("welcome" + args[0]);
+     System.out.println(args[1]);
+    }
+ }
---
+++ jdoodle.java
@@ -0,0 +1,6 @@
+class A {
+    public static void main(String args[]) {
+     System.out.println(args[0] + "technologies");
+     System.out.println(args[1]);
+    }
+}
